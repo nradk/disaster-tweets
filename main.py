90c84613e65e45cf5d1137a3c9ea13e94a892fe1
Neeraj Adhikari <adh.neeraj@gmail.com>
2020-04-24 11:02:12 -0400

    Implement CNNModel as a subclass of Model
    Implement CNNModel as a subclass of Model, encapsulating the Net
    subclass of torch.nn.Module. Place CNNModel in module models.

--- main.py
+++ main.py
@@ -2,76 +2,28 @@
 
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 
 import preprocess
+import models
 
 Xtrain, ytrain = None, None
 Xtest = None
 
-Xtrain, ytrain  = preprocess.load_and_preprocess_train(use_saved_vectors=True)
+Xtrain, ytrain = preprocess.load_and_preprocess_train(use_saved_vectors=True)
 
 # Convert the numpy arrays to torch vectors
-X, y = torch.from_numpy(Xtrain[:6000,:,:]), torch.from_numpy(ytrain[:6000])
-Xv, yv = torch.from_numpy(Xtrain[6000:,:,:]), torch.from_numpy(ytrain[6000:])
+X, y = torch.from_numpy(Xtrain[:6000, :, :]), torch.from_numpy(ytrain[:6000])
+Xv, yv = torch.from_numpy(Xtrain[6000:, :, :]), torch.from_numpy(ytrain[6000:])
 
 
 N = X.size()[0]
 L = X.size()[1]
 C = X.size()[2]
 
-class Net(nn.Module):
+cnn_model = models.CNNModel(channels=C, size=L)
+cnn_model.learn(X, y)
 
-    def __init__(self):
-        super(Net, self).__init__()
-        # First convolution layer.  C input channels, 4 output channels and
-        # convolution kernel size 2.
-        self.conv1 = nn.Conv1d(C, 4, 3, padding=1)
-        # Second convolution layer.  4 input channels from the previous layer,
-        # 12 output channels and convolution kernel size 3.
-        self.conv2 = nn.Conv1d(4, 8, 5, padding=2)
-        # Fuly connected layers
-        self.fc1 = nn.Linear(8 * L, 40)
-        self.fc2 = nn.Linear(40, 2)
-        # self.sm = nn.Softmax(dim=0)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-
-    def num_flat_features(self, x):
-        size = x.size()[1:]     # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
-
-net = Net()
-print(net)
-net.float()
-net.zero_grad()
-loss_function = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters())
-
-EPOCHS = 1
-print("Starting training")
-for epoch in range(EPOCHS):
-    for i in range(N):
-        optimizer.zero_grad()
-        target = torch.unsqueeze(y[i], 0)
-        output = net(torch.unsqueeze(torch.transpose(X[i,:], 0, 1), 0).float())
-        loss = loss_function(output, target)
-        loss.backward()
-        optimizer.step()
-    print("Epoch {} done".format(epoch))
-
-_, predicted = torch.max(net(torch.transpose(Xv, 1, 2).float()), 1)
+predicted = cnn_model.infer(Xv)
 total = yv.size()[0]
 correct = (predicted == yv).sum().item()
 print("Correct =", correct)
--- models.py
+++ models.py
@@ -1,3 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
 class Model:
     """
     Base class for machine learning models
@@ -6,7 +12,7 @@ class Model:
     methods `learn()` and `infer()`.
     """
 
-    def learn(input_data, labels):
+    def learn(self, input_data, labels):
         """
         Learn a representation from the given input data and labels.
 
@@ -15,7 +21,7 @@ class Model:
         """
         pass
 
-    def infer(input_data):
+    def infer(self, input_data):
         """
         Use the model to perform inference on input.
 
@@ -23,3 +29,70 @@ class Model:
         :returns: A pytorch tensor containing inference outputs.
         """
         pass
+
+
+class CNNModel(Model):
+    """
+    Implements a Convolutional Neural Network
+
+    Implements a small convolutional neural network with two convolutional
+    layers and two fully connected layers. See inner class `Net` for
+    implementation details.
+    """
+
+    class Net(nn.Module):
+
+        def __init__(self, channels, size):
+            super(CNNModel.Net, self).__init__()
+            # First convolution layer.  C input channels, 4 output channels and
+            # convolution kernel size 3.
+            self.conv1 = nn.Conv1d(channels, 4, 3, padding=1)
+            # Second convolution layer. 4 input channels from the previous
+            # layer, 8 output channels and convolution kernel size 5.
+            self.conv2 = nn.Conv1d(4, 8, 5, padding=2)
+            # Fuly connected layers
+            self.fc1 = nn.Linear(8 * size, 40)
+            self.fc2 = nn.Linear(40, 2)
+
+        def forward(self, x):
+            x = F.relu(self.conv1(x))
+            x = F.relu(self.conv2(x))
+            x = x.view(-1, self.num_flat_features(x))
+            x = F.relu(self.fc1(x))
+            x = self.fc2(x)
+            return x
+
+        def num_flat_features(self, x):
+            size = x.size()[1:]     # all dimensions except the batch dimension
+            num_features = 1
+            for s in size:
+                num_features *= s
+            return num_features
+
+    def __init__(self, channels, size):
+        self.net = self.Net(channels, size)
+        self.net.float()
+        self.loss_function = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(self.net.parameters())
+        self.EPOCHS = 1
+
+    def learn(self, input_data, labels):
+        self.net.zero_grad()
+        print("Starting CNN training.")
+        X, y = input_data, labels
+        N = input_data.size()[0]
+        for epoch in range(self.EPOCHS):
+            for i in range(N):
+                self.optimizer.zero_grad()
+                target = torch.unsqueeze(y[i], 0)
+                transposed_input = torch.transpose(X[i, :], 0, 1)
+                output = self.net(torch.unsqueeze(transposed_input, 0).float())
+                loss = self.loss_function(output, target)
+                loss.backward()
+                self.optimizer.step()
+            print("Epoch {} done".format(epoch))
+
+    def infer(self, input_data):
+        X = input_data
+        _, predicted = torch.max(self.net(torch.transpose(X, 1, 2).float()), 1)
+        return predicted
